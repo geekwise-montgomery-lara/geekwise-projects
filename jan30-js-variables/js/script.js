7935ccb972eb2e2dd322f98d0e4b7c109d8906ff
Gus <geekwise.gustavo.jimenez@gmail.com>
2017-01-30 15:50:36 -0800

    js start

---
+++ jan30-js-variables/js/script.js
@@ -0,0 +1,21 @@
+var what_is_the_meaning_of_life = 42
+
+
+var myNumberVar = 69; //Number Object
+var myFloatVar = 4.2; //Float Object
+var myStringVar = 'Monty Lara'; //String Object
+var myBooleanVar = true; //Boolean Object
+var myArrayVar = ['one', 'stingray', 'banana', 42, true]; //Array Object
+var myObjectVar = {  //Object Object
+    make: 'Truck',
+    model: 'F150',
+    color: 'Red',
+    year: 1999
+};
+
+console.log(typeof myNumberVar);
+console.log(typeof myStringVar);
+console.log(typeof myBooleanVar);
+console.log(typeof myFloatVar);
+console.log(typeof myArrayVar);
+console.log(myObjectVar.model);
