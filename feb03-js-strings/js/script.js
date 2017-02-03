65149112e632aced792b026a12ab933646832d05
Gus <geekwise.gustavo.jimenez@gmail.com>
2017-02-03 15:47:45 -0800

    js strings, feb02

---
+++ feb03-js-strings/js/script.js
@@ -0,0 +1,25 @@
+var what_is_the_meaning_of_life = 42;
+
+var ogString = "og string";
+var firstString = "A literal string";
+var secondString = new String('A string object');
+var firstName = "Montgomery"
+var lastName = "Lara"
+var ageNumber = 17
+
+var concatString = firstString + ', ' + secondString;
+var nameString = firstName + ' J. ' + lastName;
+var birthYear = 1999;
+var age2020 = 2020 - birthYear;
+var fullSen = 'My name is ' + nameString + '. I was born on ' + birthYear + ' and in 2020, I will be ' + age2020 + '.'
+
+// console.log(concatString);
+// console.log(nameString);
+console.log(fullSen);
+
+//+
+//concatenation
+//concatenator
+
+var getIndex = firstString.match('it');
+//console.log(getIndex);
