428cc75e06ee2e908c3d841473daca24e88de4cb
Gus <geekwise.gustavo.jimenez@gmail.com>
2017-03-14 20:34:23 -0700

    added username to portfolio

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var what_is_the_meaning_of_life = 42;
+
+
+var h1 = document.querySelector('#intro h1');
+var welcome = document.querySelector('#home section:nth-of-type(1) h1');
+
+btn.addEventListener('click', function(evt){
+    evt.preventDefault();
+    var userName = document.querySelector('#intro input[type="text"]').value;
+    localStorage.setItem('user', userName);
+    if(userName.trim() === ''){
+        h1.textContent = 'Please, type your name.';
+    }else{
+        window.location.href='/home.html';
+    }
+});
